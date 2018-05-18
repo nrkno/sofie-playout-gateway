c37b86516c6c3fbef05687c69aeda7f04b31f013
Johan Nyman <johan@nytamin.se>
2018-05-18 16:11:37 +0200

    started on CLI (WIP)

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,24 @@
 import {Connector, Config} from './connector'
+// import * as Winston from 'winston'
+
+// // CLI arguments / Environment variables --------------
+// let host: string 		= process.env.CORE_HOST 					|| '127.0.0.1'
+// let port: number 		= parseInt(process.env.CORE_PORT + '', 10) 	|| 3000
+// let logPath: string 	= process.env.CORE_LOG						|| ''
+
+// logPath = logPath
+
+// let prevProcessArg = ''
+// process.argv.forEach((val) => {
+// 	if (prevProcessArg.match(/-host/i)) {
+// 		host = val
+// 	} else if (prevProcessArg.match(/-port/i)) {
+// 		port = parseInt(val, 10)
+// 	} else if (prevProcessArg.match(/-log/i)) {
+// 		logPath = val
+// 	}
+// 	prevProcessArg = val + ''
+// })
 
 console.log('------------------------------------------------------------------')
 console.log('Starting Playout Gateway')
