28b4d86e6d76f649d58affdf1aada331ee08b715
Johan Nyman <johan@nytamin.se>
2018-05-22 14:25:26 +0200

    chore: added dependency, rename & linting

--- src/mediaScanner.ts
+++ src/mediaScanner.ts
@@ -4,8 +4,8 @@ export interface ScannerConfig {
 }
 
 export enum MediaStreamType {
-	Audio = "audio",
-	Video = "video",
+	Audio = 'audio',
+	Video = 'video'
 }
 
 export interface MediaStreamCodec {
@@ -33,7 +33,7 @@ export interface MediaStream {
 	channels?: number
 	channel_layout?: string
 	bits_per_sample?: number
-	
+
 	// common
 	time_base?: string
 	start_time?: string
@@ -54,10 +54,10 @@ export interface MediaFormat {
 }
 
 export enum FieldOrder {
-	Unknown = "unknown",
-	Progressive = "progressive",
-	TFF = "tff",
-	BFF = "bff",
+	Unknown = 'unknown',
+	Progressive = 'progressive',
+	TFF = 'tff',
+	BFF = 'bff'
 }
 
 export interface MediaInfo {
@@ -86,7 +86,7 @@ export interface MediaObject {
 
 	previewSize?: number
 	previewTime?: number
-	
+
 	cinf: string // useless to us
 	tinf: string // useless to us
 
@@ -99,7 +99,7 @@ export interface MediaObject {
 /**
  * Represents a connection between Gateway and Media-Scanner
  */
-export class ScannerHandler {
+export class MediaScanner {
 	private _config: ScannerConfig
 	private _db: PouchDB.Database
 	private _changes: PouchDB.Core.Changes<MediaObject>
@@ -119,9 +119,9 @@ export class ScannerHandler {
 		// return core.call('getMySequenceNumber', someDeviceId, (sequenceNr) => {
 		{
 			let sequenceNr: any = 185
-			if (sequenceNr < 0)
+			if (sequenceNr < 0) {
 				sequenceNr = 'now'
-
+			}
 			// Listen for changes
 			this._changes = this._db.changes<MediaObject>({
 				since: sequenceNr,
@@ -141,13 +141,13 @@ export class ScannerHandler {
 					// Note: it only exists if there is a previewTime or previewSize set in the doc
 				}
 			}).on('error', (err) => {
-				if (err.code == "ECONNREFUSED"){
+				if (err.code === 'ECONNREFUSED') {
 					// TODO
-					console.log("Connection refused")
+					console.log('Connection refused')
 				} else if (err instanceof SyntaxError) {
-					console.log("Connection terminated") // most likely
+					console.log('Connection terminated') // most likely
 				} else {
-					console.log("Error", err)
+					console.log('Error', err)
 				}
 
 				this._changes.cancel()
@@ -160,9 +160,16 @@ export class ScannerHandler {
 			console.log('scanner init done')
 		})
 	}
-	
+
 	public scrapeAll (): Promise<any> {
 		return this.scrapePage('', 100)
+	}
+	public destroy (): Promise<void> {
+		if (this._changes) {
+			this._changes.cancel()
+		}
+
+		return this._db.close()
 	}
 
 	private scrapePage (startKey: string, limit: number): Promise<any> {
@@ -175,19 +182,12 @@ export class ScannerHandler {
 					const md: MediaObject = doc.doc
 					console.log('updateMediaObject', someDeviceId, md, -1)
 				}
-			});
+			})
 
 			// Note: Also drop this block if paging isnt wanted
-			if (docs.rows.length == limit)
+			if (docs.rows.length === limit) {
 				return this.scrapePage(docs.rows[docs.rows.length - 1].id, limit)
+			}
 		})
-	}
-
-	destroy (): Promise<void> {
-		if (this._changes) {
-			this._changes.cancel()
-		}
-
-		return this._db.close()
 	}
 }
